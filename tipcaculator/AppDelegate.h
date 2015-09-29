0021bcae5cc2373242b119a1e26dda012198bd42
Shweta Turakhia <sturakhia@groupon.com>
2015-09-29 14:34:16 -0700

    Initial Commit

---
+++ tipcaculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcaculator
+//
+//  Created by Shweta Turakhia on 9/29/15.
+//  Copyright (c) 2015 groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcaculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcaculator
+//
+//  Created by Shweta Turakhia on 9/29/15.
+//  Copyright (c) 2015 groupon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
